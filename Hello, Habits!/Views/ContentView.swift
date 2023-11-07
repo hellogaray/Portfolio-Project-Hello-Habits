afb0a40897cd7026bfa0a66e106b7fe8496d0e11
hellogaray <137473678+hellogaray@users.noreply.github.com>
2023-11-07 13:42:00 +0900

    Hello Habits v.01

---
+++ Hello, Habits!/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Hello, Habits!
+//
+//  Created by Leonel Garay on 11/7/23.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+       HomeView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
